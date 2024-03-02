0bc7d6f307051ecb9793bbd140f08472e0ee18bd
Samuel0206 <xiachuangjian@gmail.com>
2024-03-03 01:34:48 +0700

    Reload

--- app/src/main/java/com/project/tracker/fragment/MainPageFragment.kt
+++ app/src/main/java/com/project/tracker/fragment/MainPageFragment.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ListView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.project.tracker.R
+import com.project.tracker.databinding.ActivityMainPageFragmentBinding
 
 class MainPageFragment : Fragment() {
-
+    private lateinit var view: ActivityMainPageFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return view.root
+    }
 }
